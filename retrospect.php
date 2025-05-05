1753a155e19567d019a677184567afc66185b107
DarkNess1903 <nes0953575427@gmail.com>
2025-05-05 13:57:27 +0700

    Update retrospect.php

--- retrospect.php
+++ retrospect.php
@@ -81,6 +81,7 @@ $conn->close();
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
 <!-- Include Top Navbar -->
